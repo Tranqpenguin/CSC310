3aca557974dcc483c22b61e43e91fb85b0fb8527
Andre Quiroa <andrejquiroa@gmail.com>
2022-02-25 00:12:47 -0500

    finsihed java
    yep

--- IdeaProjects/Rational/src/com/company/Rational.java
+++ IdeaProjects/Rational/src/com/company/Rational.java
@@ -3,40 +3,54 @@ package com.company;
 import java.util.Objects;
 
 public class Rational {
-    private final float numerator;
-    private final float denominator;
 
-    private double doubleValue(int number) {
-        double newDouble = number;
-        return newDouble;
-    }
+    float p1;
+    float p2;
+    // constructor
     public Rational(float p1, float p2) {
-        this.numerator = p1;
-        this.denominator = p2;
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
+    // getters
+    public float getP1() {
+        return p1;
+    }
+
+    public float getP2() {
+        return p2;
+    }
+
+    // equals() method to check two Rational is equal or not
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Rational rational = (Rational) o;
-        return Float.compare(rational.numerator, numerator) == 0 && Float.compare(rational.denominator, denominator) == 0;
+        return Float.compare(rational.p1, p1) == 0 && Float.compare(rational.p2, p2) == 0;
     }
 
     public int hashCode() {
-        return Objects.hash(numerator, denominator);
+        return Objects.hash(p1, p2);
     }
-    
+
+    // doubleValue() method to return of the getter
+    public double doubleValue(){
+        return getP1();
+    }
+
+    // Driver code
     public static void main(String[] args) {
         Rational rational12 = new Rational(1, 2);
         Rational rational23 = new Rational(2, 3);
+
         System.out.println("Test rational12 equals another Rational(1,2): " +
                 (rational12.equals(new Rational(1, 2)) ? "passed" : "failed"));
+
         System.out.println("Test rational12 not equals rational23: " +
                 (! rational12.equals(rational23) ? "passed" : "failed"));
+
         System.out.println("Test rational12 < rational23 as doubles: " +
                 (rational12.doubleValue() < rational23.doubleValue() ?
                         "passed" : "failed"));
     }
-
-    
 }
