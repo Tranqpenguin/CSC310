d3a8c432a2df9c515a565f45daaa1ebc87652d66
Andre Quiroa <andrejquiroa@gmail.com>
2022-03-24 19:40:31 -0400

    a8
    a8

--- IdeaProjects/Rational/test/com/company/RationalTest.java
+++ IdeaProjects/Rational/test/com/company/RationalTest.java
@@ -1,14 +1,47 @@
 package com.company;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RationalTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Test rational12 equals another Rational(1,2)")
     void testEquals() {
+        Rational rational12 = new Rational(1, 2);
+        assertTrue(rational12.equals(new Rational(1, 2)));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void testNotEquals(){
+        Rational rational12 = new Rational(1, 2);
+        Rational rational23 = new Rational(2, 3);
+        assertFalse(rational12.equals(rational23));
+    }
+
+
+    @Test
+    @DisplayName("Rational(2,3) is equal to approximately 0.666667")
     void doubleValue() {
+        Rational rational23 = new Rational(2, 3);
+        assertEquals(0.666667, rational23.doubleValue(), 0.0001);
+    }
+
+    @Test
+    @DisplayName("checking to see of object is not comparable")
+    void cantCompareNull() {
+        Rational rational12 = new Rational(1, 2);
+        assertThrows(NullPointerException.class, () -> rational12.equals(null));
+    }
+
+    @Test
+    @DisplayName("Checking to see if denominator is equal to zero")
+    void zeroDenominatorIsIllegal(){
+        assertThrows(IllegalArgumentException.class, () -> new Rational(1,0));
     }
 }
